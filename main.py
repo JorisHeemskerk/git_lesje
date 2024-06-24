3e5caa3a1ba34cafc02589f38e7f51a83d524f27
JorisHeemskerk <joris.heemskerk@student.hu.nl>
2024-06-24 10:31:28 +0200

    remove !

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 def hello_world()-> str:
     """
-    This function returns the string 'Hello World!'
+    This function returns the string 'Hello World'
 
-    @return string with 'Hello World!'
+    @return string with 'Hello World'
     """
-    return "Hello World!"
+    return "Hello World"
 
 def main()-> None:
     print(hello_world())

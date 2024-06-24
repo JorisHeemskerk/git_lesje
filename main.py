879b928810cfee900c37b40cef718bf30d49c0cc
JorisHeemskerk <joris.heemskerk@student.hu.nl>
2024-06-24 10:17:03 +0200

    added docstring

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import numpy as np
 
 def helloWorld()-> str:
+    """
+    This function returns the string 'Hello World!'
+
+    @return string with 'Hello World!'
+    """
     return "Hello World!"
 
 def main()-> None:
